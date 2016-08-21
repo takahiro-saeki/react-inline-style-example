029053b02094d543f79ef4e157f3434e3cf7fad5
takahiro-saeki <hiroooon69@gmail.com>
2016-08-21 13:51:25 +0900

    commit.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 #React-inline-style-example
 this is a sample of inline-style with React.js.
+sample is here.
+
+##USAGE
+```
+$ npm i
+```
+
+```
+$ npm start
+```
--- assets/js/data/imgPath.js
+++ assets/js/data/imgPath.js
@@ -6,7 +6,7 @@ const imgPath = [
     path: 'https://github.com/takahiro-saeki/img-bank/raw/master/react-inline-style-example/cat2.png'
   },
   {
-    path: 'https://github.com/takahiro-saeki/img-bank/raw/master/react-inline-style-example/cat1.png'
+    path: 'https://github.com/takahiro-saeki/img-bank/raw/master/react-inline-style-example/cat7.png'
   },
   {
     path: 'https://github.com/takahiro-saeki/img-bank/raw/master/react-inline-style-example/cat2.png'
@@ -15,7 +15,7 @@ const imgPath = [
     path: 'https://github.com/takahiro-saeki/img-bank/raw/master/react-inline-style-example/cat1.png'
   },
   {
-    path: 'https://github.com/takahiro-saeki/img-bank/raw/master/react-inline-style-example/cat2.png'
+    path: 'https://github.com/takahiro-saeki/img-bank/raw/master/react-inline-style-example/cat7.png'
   }
 ]
 
